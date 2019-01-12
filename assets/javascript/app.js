290407e6c8608df8401a413d1eaa0f1042609515
schnibb <schnibbend@gmail.com>
2019-01-12 11:17:13 -0500

    added firebase config to app.js as well as firebase script to index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,10 @@
-hello world
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDbwHgPqmcP1dWVG82WWrrla9HhkrL1hnY",
+    authDomain: "my-little-princess-dresses.firebaseapp.com",
+    databaseURL: "https://my-little-princess-dresses.firebaseio.com",
+    projectId: "my-little-princess-dresses",
+    storageBucket: "my-little-princess-dresses.appspot.com",
+    messagingSenderId: "758916304123"
+};
+firebase.initializeApp(config);
