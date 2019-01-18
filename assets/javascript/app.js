5ff891ff33638ce9e673ab55e3aba7cc92bc1ed3
schnibb <schnibbend@gmail.com>
2019-01-17 20:05:32 -0500

    changes made to the app.js page adding in code to pull from firebase and append to page.  Also created products.html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,26 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDbwHgPqmcP1dWVG82WWrrla9HhkrL1hnY",
-    authDomain: "my-little-princess-dresses.firebaseapp.com",
-    databaseURL: "https://my-little-princess-dresses.firebaseio.com",
-    projectId: "my-little-princess-dresses",
-    storageBucket: "my-little-princess-dresses.appspot.com",
-    messagingSenderId: "758916304123"
-};
-firebase.initializeApp(config);
+$(document).ready(function() {
+
+    console.log("connected");
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDbwHgPqmcP1dWVG82WWrrla9HhkrL1hnY",
+        authDomain: "my-little-princess-dresses.firebaseapp.com",
+        databaseURL: "https://my-little-princess-dresses.firebaseio.com",
+        projectId: "my-little-princess-dresses",
+        storageBucket: "my-little-princess-dresses.appspot.com",
+        messagingSenderId: "758916304123"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    // Loop through users in order with the forEach() method. The callback
+    // provided to forEach() will be called synchronously with a DataSnapshot
+    // for each child:
+    var query = database.ref().orderByKey();
+    console.log(query);
+    query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                $("#dress-information").append("<div class = 'col-sm-6'><div class = 'card mx-auto' style = 'width: 18rem;'><img src = '" + childSnapshot.val().image + "' class = 'card-img-top' alt = '...' ><div class = 'card-body'> <h5 class = 'card-title dress' id = 'dress'>" + childSnapshot.val().model_id + "</h5></div><ul class='list-group list-group-flush'><li class='list-group-item description' id='description'>" + childSnapshot.val().description + "</li><li class='list-group-item line' id='line'>" + childSnapshot.val().line + "</li><li class='list-group-item quantity' id='quantity'>" + childSnapshot.val().quantity + "</li></ul><div class= 'card-body'><a href='cart.html' class='btn btn-primary'>BUY</a></div></div></div>");
+            });
+        });
+})
